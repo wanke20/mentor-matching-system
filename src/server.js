2d5d3fb515ea5ba931c2a33a2dd3ba3cb85537cd
wanke20 <122842952+wanke20@users.noreply.github.com>
2023-07-22 00:05:40 -0400

    update package.json

--- src/server.js
+++ src/server.js
@@ -10,36 +10,36 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", async (request, response) => {
-  response.send(await Participant.find());
+    response.send(await Participant.find());
 });
 
 app.put("/", async (request, response) => {
-  Product.findOne({ _id: request.params.id }, (err, product) => {
-    if (err) {
-      response.status(500).send(err);
-    } else {
-      product.name = request.body.name;
-      product.price = request.body.price;
-      product.save((err) => {
+    Product.findOne({ _id: request.params.id }, (err, product) => {
         if (err) {
-          response.status(500).send(err);
+            response.status(500).send(err);
         } else {
-          response.send(product);
+            product.name = request.body.name;
+            product.price = request.body.price;
+            product.save((err) => {
+                if (err) {
+                    response.status(500).send(err);
+                } else {
+                    response.send(product);
+                }
+            });
         }
-      });
-    }
-  });
+    });
 });
 
 app.post("/", async (request, response) => {
-  var participant = new Participant();
-  participant.name = request.body.name;
-  participant.description = request.body.description;
-  participant.save((err) => {
-    if (err) {
-      response.status(500).send(err);
-    } else {
-      response.send(participant);
-    }
-  });
+    var participant = new Participant();
+    participant.name = request.body.name;
+    participant.description = request.body.description;
+    participant.save((err) => {
+        if (err) {
+            response.status(500).send(err);
+        } else {
+            response.send(participant);
+        }
+    });
 });
