5e7b00cfc2c822d7302497ed580493a25bed55a7
wanke20 <122842952+wanke20@users.noreply.github.com>
2023-07-22 00:10:45 -0400

    Update server.js

--- src/server.js
+++ src/server.js
@@ -1,9 +1,9 @@
 import express from "express";
-var app = express();
+let app = express();
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 
-var Participant = mongoose.model("Participant");
+let Participant = mongoose.model("Participant");
 var Mentor = mongoose.model("Mentor");
 
 app.use(bodyParser.json());
